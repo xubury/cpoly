0d714b7151a237196b8d0274dd427decf77a60fd
xubury <xubury97@gmail.com>
2023-07-27 20:06:05 +0800

    * first draft

---
+++ cpoly.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define POLY_TYPE(name)                _Poly_##name
+#define POLY_VAR(var)                  _poly_##var
+#define POLY_INTERFACE(name)           _Poly_Func_##name
+#define POLY_PROTOTYPE(ret, name, ...) typedef ret (*POLY_TYPE(name))(__VA_ARGS__)
+
+#define POLY_IMPL(name, impl)                \
+    POLY_TYPE(name) _impl_##name     = impl; \
+    interfaces[POLY_INTERFACE(name)] = (void *)_impl_##name;
+
+typedef struct {
+    char const *const type;
+    void *const      *interfaces;
+} POLY_TYPE(table);
+
+#define POLY_DERIVED(base, derived, ctor, impls)                                              \
+    base ctor(derived *ptr)                                                                   \
+    {                                                                                         \
+        static void *interfaces[POLY_TYPE(base)] = {0};                                       \
+        {                                                                                     \
+            impls                                                                             \
+        }                                                                                     \
+        static POLY_TYPE(table) const tbl = {.type = #derived, .interfaces = &interfaces[0]}; \
+        return (base){.POLY_VAR(self) = ptr, .POLY_VAR(table) = &tbl};                        \
+    }
+
+#define POLY_TABLE(base, ...)               \
+    enum {                                  \
+        __VA_ARGS__,                        \
+        POLY_TYPE(base)                     \
+    };                                      \
+    void                   *POLY_VAR(self); \
+    POLY_TYPE(table) const *POLY_VAR(table);
+
+#define poly_params(...) __VA_ARGS__
+
+#define poly_func(base, name) ((POLY_TYPE(name))base->POLY_VAR(table)->interfaces[POLY_INTERFACE(name)])
+
+#define poly_cast(base, derived) \
+    (strcmp((base)->POLY_VAR(table)->type, #derived) == 0 ? (derived *)(base)->POLY_VAR(self) : 0)
---
+++ example.c
@@ -0,0 +1,97 @@
+#include "cpoly.h"
+#include <stdio.h>
+#include <malloc.h>
+
+// base
+typedef struct {
+    int x;
+    int y;
+    // declare a vtable
+    POLY_TABLE(Context, POLY_INTERFACE(print), POLY_INTERFACE(init))
+} Context;
+
+POLY_PROTOTYPE(void, init, Context *);
+POLY_PROTOTYPE(void, print, Context *, int);
+
+// derived  1
+typedef struct {
+    float o;
+} glContext;
+
+void gl_init(Context *ctx)
+{
+    ctx->x = 1;
+    printf("gl init\n");
+}
+
+void gl_print(Context *ctx, int param)
+{
+    printf("gl print: x: %d param: %d\n", ctx->x, param);
+}
+
+POLY_DERIVED(Context, glContext, gl_ctor, POLY_IMPL(print, gl_print) POLY_IMPL(init, gl_init))
+
+// derived 2
+typedef struct {
+    int o;
+} vkContext;
+
+void vk_init(Context *ctx)
+{
+    ctx->x = 2;
+    printf("vk init\n");
+}
+
+void vk_print(Context *ctx, int param)
+{
+    printf("vk print: x: %d param: %d\n", ctx->x, param);
+}
+
+void vk_illegal(void)
+{
+}
+
+POLY_DERIVED(Context, vkContext, vk_ctor, POLY_IMPL(print, vk_print) POLY_IMPL(init, vk_init))
+
+// calling interface
+void init(Context *ctx)
+{
+    poly_func(ctx, init)(ctx);
+}
+
+void print(Context *ctx)
+{
+    poly_func(ctx, print)(ctx, 123);
+}
+
+int main(int argc, char *argv[])
+{
+    Context *ctx = malloc(sizeof(Context));
+    {
+        glContext gl = {.o = 0.123};
+        *ctx         = gl_ctor(&gl);
+        printf("%f\n", poly_cast(ctx, glContext)->o);
+        assert(poly_cast(ctx, glContext));
+        assert(poly_cast(ctx, vkContext) == NULL);
+        init(ctx);
+        print(ctx);
+    }
+    {
+        glContext gl = {.o = 0.456};
+        *ctx         = gl_ctor(&gl);
+        printf("%f\n", poly_cast(ctx, glContext)->o);
+        init(ctx);
+        print(ctx);
+    }
+    {
+        vkContext vk = {.o = 1};
+        *ctx         = vk_ctor(&vk);
+        printf("%d\n", poly_cast(ctx, vkContext)->o);
+        assert(poly_cast(ctx, vkContext));
+        assert(poly_cast(ctx, glContext) == NULL);
+        init(ctx);
+        print(ctx);
+    }
+
+    return 0;
+}
